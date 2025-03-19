56d8d0e0ced58e673c36fa5ab3fe4c47114560f6
ali mawassi <202112066@ua.edu.lb>
2025-03-19 04:44:53 +0200

    some bugs

--- routes/contact.php
+++ routes/contact.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/contact', [ContactController::class, 'showContactForm'])->name('contact');
 Route::post('/contact', [ContactController::class, 'submitForm'])->name('contact.submit');
+Route::get('/contact', [ContactController::class, 'showContactForm'])->name('contact');
+
+
+
+
